4eecc4201b5923349f305e31c6d5ab43a2e3610a
Sandy <t109598053@ntut.org.tw>
2020-08-25 17:00:29 +0800

    third logicSimulator

--- src/main/java/LogicSimulator.java
+++ src/main/java/LogicSimulator.java
@@ -3,10 +3,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Vector;
+import java.util.*;
 
 public class LogicSimulator
 {
@@ -16,13 +13,21 @@ public class LogicSimulator
     private Integer ni;
     private Integer ng;
 
-    public String getSimulationResult(Vector<Boolean> value){        //輸入
+    public LogicSimulator(){
+        this.circuits = new Vector<>();
+        this.iPins = new Vector<>();
+        this.oPins = new Vector<>();
+    }
+    public String getSimulationResult(Vector<Boolean> inputValues){        //輸入
         String result = "Simulation Result:\n" +
                 "i i i | o\n" +
                 "1 2 3 | 1\n" +
                 "------+--\n" ;
 //        +                "0 1 1 | 0\n";
 
+        for(int i=0;i<inputValues.size();i++){
+//            this.iPins.addInputPin(inputValues.get(i));
+        }
         return result;
     }
 
@@ -36,42 +41,54 @@ public class LogicSimulator
         ni = Integer.valueOf(br.readLine());//number_input
         ng = Integer.valueOf(br.readLine());//number_gate
         String str = null;
-        Vector circuits_data = new Vector();
-//        ArrayList arrayLists = new ArrayList();
-        List list = null;
-        System.out.println("list0:"+list);
+        List circuits_data = new ArrayList<List>();
+        Vector circuits_vector = new Vector();
         while ((str = br.readLine()) != null)
         {
-//            circuits_data.add(Arrays.asList(str.split(" ")));
-                    list.add(Arrays.asList(str.split(" ")));
-            System.out.println("list:"+list);
-            System.out.println("listi:"+list.get(2));
-//            System.out.println("arr:"+Arrays.asList(str.split(" ")));
-//            System.out.println("arri:"+Arrays.asList(str.split(" ")).get(2));
-            System.out.println("\n");
-//            this.circuits.add(new Vector<>(Arrays.asList(str.split(" "))));
+            circuits_data = Arrays.asList(str.split(" "));  //[1, -1, 2.1, 3.1, 0]
+            circuits_vector.add(circuits_data);  //[[1, -1, 2.1, 3.1, 0], [3, -2, 0], [2, 2.1, -3, 0]]
+            //buildGates
+            String circuit_type = circuits_data.get(0).toString();
+            switch (circuit_type)
+            {
+                case "1":
+                    circuits.add(new GateAND());
+                    break;
+                case "2":
+                    circuits.add(new GateOR());
+                    break;
+                case "3":
+                    circuits.add(new GateNOT());
+                    break;
+            }
         }
         br.close();
         fr.close();
 
-        System.out.println("circuits_data:"+ circuits_data);
 
-        int data_size = circuits_data.size();
-        for(int i=0;i<data_size;i++){
-            List data_lists = Arrays.asList(circuits_data.get(i));
-            System.out.println("circuits_data.gets:"+data_lists);
-            int dl_size = data_lists.size();
-            for(int j=0;j<dl_size;j++){
-                String data = data_lists.get(j).toString();
-                System.out.println("data:"+data_lists.get(j));
+        int cv_size = circuits_vector.size();
+        for(int i=0;i<cv_size;i++){
+            circuits_data = (List) circuits_vector.get(i);
+            System.out.println("circuits_data.gets:"+circuits_data);
+            int j=1;
+            while(!circuits_data.equals("0")){
+                String data = circuits_data.get(j).toString();
+                System.out.println("circuits_data:"+circuits_data);
+                System.out.println("datai:"+data);
                 if(data.contains(".")){
-//                    int dot_index = arrayLists.get(i).toString().indexOf(".");
-//                    System.out.println("al"+i+":"+dot_index);
-//                    System.out.println("data:"+data);
+                    int op = Integer.valueOf(data);
+                    circuits.get(circuits.size()-1).addInputPin(oPins.get(op-1));
+//                    oPinUsedTimes.set(op-1, oPinUsedTimes.get(op-1)+1);
                 }
+                else if(data.contains("-")){
+                    int ip = (-1) * Integer.parseInt(data);
+                    circuits.get(circuits.size()-1).addInputPin(iPins.get(ip-1));
             }
         }
-//        if()
+                j++;
+            }
         return true;
+////        if()
     }
+
 }
